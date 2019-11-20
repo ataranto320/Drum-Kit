525dfd0ba3d536a2afe8d79ff6ba3ee372dd2911
Austin T <austin.taranto@gmail.com>
2019-11-20 17:44:09 -0500

    added sound and visuals for pressing keys

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
 window.addEventListener("keydown", function(e) {
-    console.log(e);
+    // console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    // console.log(audio);
+
+    // stop the function from running all together
+    if(!audio) return; 
+
+    // rewind to the start for repeated playing
+    audio.currentTime = 0
+    audio.play();
+
+    // console.log(key);
+    key.classList.add("playing");
 });
